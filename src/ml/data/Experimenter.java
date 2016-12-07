fc8ddb453b8a2143f50d87977e9c6ca6b7837c58
Dmitriy Smirnov <dimkasmir@gmail.com>
2016-12-06 19:04:32 -0800

    add experiments

--- src/ml/data/Experimenter.java
+++ src/ml/data/Experimenter.java
@@ -9,60 +9,38 @@ import ml.classifiers.*;
  */
 public class Experimenter {
 	public static void main(String[] args) {
-		DataSet dataset = new DataSet("wines.train", DataSet.TEXTFILE);
-//		DataSet dataset = new DataSet("titanic-train.csv", DataSet.CSVFILE);
+		DataSetSplit splits;
 
-		DecisionTreeClassifier d = new DecisionTreeClassifier();
-		d.setDepthLimit(5);
-		System.out.println("Decision Tree");
-		System.out.println("test acc train acc");
-		DataSetSplit splits = dataset.split(0.8);
-		double[] accuracies = calcAccuracy(dataset, d, splits);
-		System.out.format("%-10.4f%.4f%n", accuracies[0], accuracies[1]);
-		
-		EnsembleTreeClassifier etc = new EnsembleTreeClassifier();
-		System.out.println("Tree Bagging");
-		System.out.println("test acc train acc");
-		accuracies = calcAccuracy(dataset, etc, splits);
-		System.out.format("%-10.4f%.4f%n", accuracies[0], accuracies[1]);	
-		
-		etc.setFeatureBagging(true);
-		System.out.println("Feature Bagging");
-		System.out.println("depth test acc train acc");
-		accuracies = calcAccuracy(dataset, etc, splits);
-		System.out.format("%-10.4f%.4f%n", accuracies[0], accuracies[1]);
-		
-		etc.setExtraTrees(true);
-		System.out.println("ExtraTrees");
-		System.out.println("depth test acc train acc");
-		accuracies = calcAccuracy(dataset, etc, splits);
-		System.out.format("%-10.4f%.4f%n", accuracies[0], accuracies[1]);
-	}
+		for (DataSet dataset : new DataSet[] {new DataSet("wines.train", DataSet.TEXTFILE), new DataSet("titanic-train.csv", DataSet.CSVFILE)}) {
+			System.out.println("num trees  depth  test acc  train acc  tb test acc  tb train acc  fb test acc  fb train acc  et test acc  et train acc");
+			for (int depth = 1; depth <= 5; depth++) {
+				DecisionTreeClassifier d = new DecisionTreeClassifier();
+				d.setDepthLimit(depth);
+				splits = dataset.split(0.8);
+				double[] accuracies = calcAccuracy(dataset, d, splits);
+				
+				for (int numTrees = 10; numTrees <= 50; numTrees += 10) {
+					EnsembleTreeClassifier etc = new EnsembleTreeClassifier();
+					etc.setDepthLimit(depth);
+					etc.setNumTrees(numTrees);
 
-	/**
-	 * Computes n-fold validation for a given classifier
-	 * @param cvs cross validation set
-	 * @param classifier classifier to train and classify an example
-	 */
-	private static void computeFolds(CrossValidationSet cvs, Classifier classifier) {
-		double trainAcc = 0, testAcc = 0;
-		for (int s = 0; s < cvs.getNumSplits(); s++) {
-			DataSet train = cvs.getValidationSet(s).getTrain();
-			classifier.train(train);
-			
-			int correct = 0;
-			for (Example e : cvs.getValidationSet(s).getTrain().getData()) {
-				if (e.getLabel() == classifier.classify(e)) correct++;
+					double[] tbAccuracies = calcAccuracy(dataset, etc, splits);
+					etc = new EnsembleTreeClassifier();
+					etc.setDepthLimit(depth);
+					etc.setNumTrees(numTrees);
+					etc.setFeatureBagging(true);
+					double[] fbAccuracies = calcAccuracy(dataset, etc, splits);
+					etc = new EnsembleTreeClassifier();
+					etc.setDepthLimit(depth);
+					etc.setNumTrees(numTrees);
+					etc.setExtraTrees(true);
+					double[] etAccuracies = calcAccuracy(dataset, etc, splits);
+					
+					System.out.format("%-11d%-7d%-10.4f%-11.4f%-13.4f%-14.4f%-13.4f%-14.4f%-13.4f%-14.4f%n", numTrees, depth, accuracies[0], accuracies[1], tbAccuracies[0], tbAccuracies[1], fbAccuracies[0], fbAccuracies[1], etAccuracies[0], etAccuracies[1]);
+				}
 			}
-			trainAcc += correct * 1.0 / cvs.getValidationSet(s).getTrain().getData().size();
-
-			correct = 0;
-			for (Example e : cvs.getValidationSet(s).getTest().getData()) {
-				if (e.getLabel() == classifier.classify(e)) correct++;
-			}
-			testAcc += correct * 1.0 / cvs.getValidationSet(s).getTest().getData().size();
+			System.out.println("-----------------------------");
 		}
-		System.out.format("%.4f %.4f%n", trainAcc / cvs.getNumSplits(), testAcc / cvs.getNumSplits());
 	}
 
 
@@ -91,16 +69,4 @@ public class Experimenter {
 
 		return new double[] {correctTest / totalTest, correctTrain / totalTrain};
 	}
-	
-	private static double calcAccuracyWithMajority(DataSet data, DecisionTreeClassifier classifier) {
-		classifier.train(data);
-		double majorityLabel = classifier.getMajorityLabel(data.getData()).majorityLabel;
-		double correct = 0, total = 0;
-		for (Example e : data.getData()) {
-			if (classifier.classify(e) == majorityLabel) correct++;
-			total++;
-		}
-		return correct / total; 
-	}
-
 }
